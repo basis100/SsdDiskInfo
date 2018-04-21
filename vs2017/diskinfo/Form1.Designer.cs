7f68e4240758cd63eef4e81b18405eafd01348ef
basis100 <basis100@qq.com>
2018-04-21 19:07:04 +0800

    first

---
+++ vs2017/diskinfo/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace diskinfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel5 = new System.Windows.Forms.ToolStripLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStripLabel6 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel2,
+            this.toolStripLabel3,
+            this.toolStripLabel4,
+            this.toolStripLabel5,
+            this.toolStripLabel6});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(777, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(35, 22);
+            this.toolStripLabel1.Text = "C:    ";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(37, 22);
+            this.toolStripLabel2.Text = "D:    ";
+            this.toolStripLabel2.Click += new System.EventHandler(this.toolStripLabel2_Click);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(34, 22);
+            this.toolStripLabel3.Text = "E:    ";
+            this.toolStripLabel3.Click += new System.EventHandler(this.toolStripLabel3_Click);
+            // 
+            // toolStripLabel4
+            // 
+            this.toolStripLabel4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripLabel4.Name = "toolStripLabel4";
+            this.toolStripLabel4.Size = new System.Drawing.Size(38, 22);
+            this.toolStripLabel4.Text = "F:     ";
+            this.toolStripLabel4.Click += new System.EventHandler(this.toolStripLabel4_Click);
+            // 
+            // toolStripLabel5
+            // 
+            this.toolStripLabel5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripLabel5.Name = "toolStripLabel5";
+            this.toolStripLabel5.Size = new System.Drawing.Size(36, 22);
+            this.toolStripLabel5.Text = "G:    ";
+            this.toolStripLabel5.Click += new System.EventHandler(this.toolStripLabel5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(777, 501);
+            this.textBox1.TabIndex = 7;
+            // 
+            // toolStripLabel6
+            // 
+            this.toolStripLabel6.Name = "toolStripLabel6";
+            this.toolStripLabel6.Size = new System.Drawing.Size(53, 22);
+            this.toolStripLabel6.Text = "readme";
+            this.toolStripLabel6.Click += new System.EventHandler(this.toolStripLabel6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(777, 526);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "SsdDiskInfo";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel4;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel6;
+    }
+}
+
---
+++ vs2017/diskinfo/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace diskinfo
+{
+
+
+
+    
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        public bool StartProcess(string filename, string[] args)
+        {
+            try
+            {
+                string s = "";
+                foreach (string arg in args)
+                {
+                    s = s + arg + " ";
+                }
+                s = s.Trim();
+                Process myprocess = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo(filename, s);
+                myprocess.StartInfo = startInfo;
+             
+
+
+
+                myprocess.StartInfo.UseShellExecute = false;
+                myprocess.StartInfo.RedirectStandardInput = true;
+                myprocess.StartInfo.RedirectStandardOutput = true;
+                myprocess.StartInfo.RedirectStandardError = true;
+                myprocess.StartInfo.CreateNoWindow = true;
+                myprocess.OutputDataReceived += da_OutputDataReceived;
+
+                myprocess.Start();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("启动应用程序时出错！原因：" + ex.Message);
+            }
+            return false;
+        }
+
+        void da_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.BeginInvoke(new Action(() => { textBox1.Text += "\r\n" + e.Data; }));
+
+
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Process da = new Process();
+            da.StartInfo.FileName = "cmd.exe";
+            da.StartInfo.UseShellExecute = false;
+            da.StartInfo.RedirectStandardInput = true;
+            da.StartInfo.RedirectStandardOutput = true;
+            da.StartInfo.RedirectStandardError = true;
+            da.StartInfo.CreateNoWindow = true;
+            da.OutputDataReceived += da_OutputDataReceived;
+            da.Start();
+            da.BeginOutputReadLine();
+            da.StandardInput.WriteLine("smartctl.exe -x c:");
+        }
+
+        private void toolStripLabel2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Process da = new Process();
+            da.StartInfo.FileName = "cmd.exe";
+            da.StartInfo.UseShellExecute = false;
+            da.StartInfo.RedirectStandardInput = true;
+            da.StartInfo.RedirectStandardOutput = true;
+            da.StartInfo.RedirectStandardError = true;
+            da.StartInfo.CreateNoWindow = true;
+            da.OutputDataReceived += da_OutputDataReceived;
+            da.Start();
+            da.BeginOutputReadLine();
+            da.StandardInput.WriteLine("smartctl.exe -x d:");
+        }
+
+        private void toolStripLabel3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Process da = new Process();
+            da.StartInfo.FileName = "cmd.exe";
+            da.StartInfo.UseShellExecute = false;
+            da.StartInfo.RedirectStandardInput = true;
+            da.StartInfo.RedirectStandardOutput = true;
+            da.StartInfo.RedirectStandardError = true;
+            da.StartInfo.CreateNoWindow = true;
+            da.OutputDataReceived += da_OutputDataReceived;
+            da.Start();
+            da.BeginOutputReadLine();
+            da.StandardInput.WriteLine("smartctl.exe -x e:");
+        }
+
+        private void toolStripLabel4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Process da = new Process();
+            da.StartInfo.FileName = "cmd.exe";
+            da.StartInfo.UseShellExecute = false;
+            da.StartInfo.RedirectStandardInput = true;
+            da.StartInfo.RedirectStandardOutput = true;
+            da.StartInfo.RedirectStandardError = true;
+            da.StartInfo.CreateNoWindow = true;
+            da.OutputDataReceived += da_OutputDataReceived;
+            da.Start();
+            da.BeginOutputReadLine();
+            da.StandardInput.WriteLine("smartctl.exe -x f:");
+        }
+
+        private void toolStripLabel5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Process da = new Process();
+            da.StartInfo.FileName = "cmd.exe";
+            da.StartInfo.UseShellExecute = false;
+            da.StartInfo.RedirectStandardInput = true;
+            da.StartInfo.RedirectStandardOutput = true;
+            da.StartInfo.RedirectStandardError = true;
+            da.StartInfo.CreateNoWindow = true;
+            da.OutputDataReceived += da_OutputDataReceived;
+            da.Start();
+            da.BeginOutputReadLine();
+            da.StandardInput.WriteLine("smartctl.exe -x g:");
+        }
+
+        private void toolStripLabel6_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://blog.csdn.net/ot512csdn/article/details/80031712");
+
+        }
+    }
+}
